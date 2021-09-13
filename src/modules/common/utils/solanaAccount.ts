dd31db0164c15233495800efe29bc49717153aae
xxy <abc_xf@126.com>
2021-09-13 18:58:50 +0800

    feat: store creators list

--- src/modules/common/utils/solanaAccount.ts
+++ src/modules/common/utils/solanaAccount.ts
@@ -1,13 +1,25 @@
-import { decodeMasterEdition, decodeMetadata, ENDPOINTS } from 'npms/oystoer';
+import {
+  decodeMasterEdition,
+  decodeMetadata,
+  ENDPOINTS,
+  METAPLEX_ID,
+  ParsedAccount,
+} from 'npms/oystoer';
 import { Connection, PublicKey } from '@solana/web3.js';
 import axios from 'axios';
+import { getProgramAccounts } from 'contexts/meta/loadAccounts';
+import { processMetaplexAccounts } from 'contexts/meta/processMetaplexAccounts';
+import {
+  MAX_WHITELISTED_CREATOR_SIZE,
+  WhitelistedCreator,
+} from 'models/metaplex';
 
 export const getAccountInfo = (publicKey: string) => {
   return axios.post(ENDPOINTS[0].endpoint, {
     method: 'getAccountInfo',
     jsonrpc: '2.0',
     params: [publicKey, { encoding: 'jsonParsed' }],
-    // TODO
+    // TODO web3 PR
     id: '5b5044f3-b12b-4904-8d79-a2affae6247b',
   });
 };
@@ -28,6 +40,7 @@ export const accountGetMasterEditionInfo = async ({
     const masterKey: string =
       nftInfo.data?.result.value.data.parsed.info.mintAuthority;
     if (masterKey) {
+      console.log('masterKey------>', masterKey);
       const masterEditionAccount = await connection.getAccountInfo(
         new PublicKey(masterKey),
       );
@@ -42,4 +55,32 @@ export const accountGetMasterEditionInfo = async ({
       }
     }
   }
+};
+
+export const getCreators = async ({
+  connection,
+}: {
+  connection: Connection;
+}) => {
+  const _creators = await getProgramAccounts(connection, METAPLEX_ID, {
+    filters: [
+      {
+        dataSize: MAX_WHITELISTED_CREATOR_SIZE,
+      },
+    ],
+  });
+  let creators: {
+    address: string;
+    account: ParsedAccount<WhitelistedCreator>;
+  }[] = [];
+  for (const creator of _creators) {
+    await processMetaplexAccounts(
+      creator,
+      (key, address, account) => {
+        creators.push({ address, account });
+      },
+      false,
+    );
+  }
+  return creators;
 };
